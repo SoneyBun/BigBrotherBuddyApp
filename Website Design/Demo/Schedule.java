d56e9a7fd0b18b7051a23d0eecde063b81a7f81f
Sunny <addrosity.yt@gmail.com>
2025-02-07 19:40:32 -0500

    Create Schedule.java

---
+++ Website Design/Demo/Schedule.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Schedule {
+    public static ArrayList<String> event = new ArrayList<>();
+    public static ArrayList<String> time = new ArrayList<>();
+    static Scanner scan = new Scanner(System.in);
+    private static int hour, minute;
+
+    public static void printSchedule() {
+        System.out.println("\nCurrent Schedule: ");
+
+        for(int i = 0; i < event.size(); i++) {
+            System.out.println("Time: " + time.get(i) + "\tEvent: " + event.get(i));
+        }
+    }
+
+    public static void modifySchedule() {
+        boolean valid = false;
+
+        while(!valid) {
+            System.out.print("\nEnter Hour: ");
+            try {
+                hour = Integer.parseInt(scan.nextLine());
+                valid = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Time!");
+            }
+        }
+        valid = false;
+        while(!valid) {
+            System.out.print("Enter Minute: ");
+            try {
+                minute = Integer.parseInt(scan.nextLine());
+                valid = true;
+                if(minute > 60) {
+                    hour += (minute/60);
+                    minute %= 60;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Time!");
+            }
+        }
+        time.add(hour + ":" + minute);
+        System.out.print("Enter Event: ");
+        event.add(scan.nextLine());
+    }
+}
